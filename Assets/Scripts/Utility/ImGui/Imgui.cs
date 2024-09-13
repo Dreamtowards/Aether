9aa72d776d4b6061dfdaaffe2dc38467c407e8c7
Dreamtowards <ref.dreamtowards@gmail.com>
2024-09-13 21:37:04 +0800

    ft: Custom Font menlo ttf

--- Assets/Scripts/Utility/ImGui/Imgui.cs
+++ Assets/Scripts/Utility/ImGui/Imgui.cs
@@ -18,19 +18,24 @@ namespace Aether
             // Unity Update method. 
             // Your code belongs here! Like ImGui.Begin... etc.
             
-            ImGui.Text($"Hello, world {123}");
-            if (ImGui.Button("Save"))
-            {
-                Debug.Log("Save");
-            }
-
-            ImGui.InputText("string", ref _inputText, 100);
-            ImGui.SliderFloat("float", ref _sliderFloatValue, 0.0f, 1.0f);
+            // ImGui.Text($"Hello, world {123}");
+            // if (ImGui.Button("Save"))
+            // {
+            //     Debug.Log("Save");
+            // }
+            //
+            // ImGui.InputText("string", ref _inputText, 100);
+            // ImGui.SliderFloat("float", ref _sliderFloatValue, 0.0f, 1.0f);
         }
 
         private void OnInitialize(UImGui.UImGui obj)
         {
             // runs after UImGui.OnEnable();
+
+            // var io = ImGui.GetIO();
+            // var font = io.Fonts.AddFontFromFileTTF("Fonts/menlo.ttf", 24.0f);
+
+            
         }
 
         private void OnDeinitialize(UImGui.UImGui obj)
@@ -49,6 +54,25 @@ namespace Aether
             UImGuiUtility.Layout -= OnLayout;
             UImGuiUtility.OnInitialize -= OnInitialize;
             UImGuiUtility.OnDeinitialize -= OnDeinitialize;
+        }
+        
+        public void AddCustomFont(ImGuiIOPtr io)
+        {
+            // you can put on StreamingAssetsFolder and call from there like:
+            //string fontPath = $"{Application.streamingAssetsPath}/NotoSansCJKjp - Medium.otf";
+            string fontPath = $"{Application.streamingAssetsPath}/Fonts/menlo.ttf";
+            io.Fonts.AddFontFromFileTTF(fontPath, 14);
+
+            // you can create a configs and do a lot of stuffs
+            //ImFontConfig fontConfig = default;
+            //ImFontConfigPtr fontConfigPtr = new ImFontConfigPtr(&fontConfig);
+            //fontConfigPtr.MergeMode = true;
+            //io.Fonts.AddFontDefault(fontConfigPtr);
+            //int[] icons = { 0xf000, 0xf3ff, 0 };
+            //fixed (void* iconsPtr = icons)
+            //{
+            //	io.Fonts.AddFontFromFileTTF("fontawesome-webfont.ttf", 18.0f, fontConfigPtr, (System.IntPtr)iconsPtr);
+            //}
         }
     }
 }
